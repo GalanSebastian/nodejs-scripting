1323f9d4043d2432001630cb808e19cf644f66e9
GalanSebastian <sebastian.galan@assist.ro>
2022-02-04 13:24:27 +0200

    Add test code

--- utils/fetch.js
+++ utils/fetch.js
@@ -7,6 +7,7 @@ function fetcher(url, settings = {}, includeCfHeaders = true) {
     console.log(process.env.API_GATEWAY)
     console.log(process.env.API_AUTH_EMAIL)
     console.log(process.env.API_AUTH_KEY)
+    console.loh(proccess.env)
 
     const defaultHeaders = {
         'X-Auth-Email': process.env.API_AUTH_EMAIL,
